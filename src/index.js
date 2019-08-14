7454cc7ee6e0c5d158485d6856a5fd1da1c78a39
Rod Glover <rglover@uvic.ca>
2019-08-14 14:35:43 -0700

    Remove makeYamlLoader

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import _ from 'lodash';
 import addMapDeep from 'deepdash/addMapDeep';
-import axios from 'axios';
-import yaml from 'js-yaml';
 
 addMapDeep(_);
 
@@ -195,33 +193,7 @@ export default class ExternalText extends React.Component {
   }
 }
 
-export function makeYamlLoader(url) {
-  // Convenience function for initializing an ExternalText Provider.
-  // Returns a function that can be used as the callback argument `loadTexts`
-  // to `ExternalTexts.Provider`. It issues an HTTP GET to `url`; treats
-  // the result as a YAML file, converting it to a JS object; then calls its
-  // argument `setTexts` with the resulting object. Any error thrown during
-  // this process is logged to the console (and `setTexts` is not called).
-  return function (setTexts) {
-    console.log('YAML loader: loading...')
-    axios.get(url, { responseType: 'text' })
-    .then(response => response.data)
-    .then(yaml.safeLoad)
-    .then(data => {
-      console.log('YAML loader: loaded', data);
-      return data;
-    })
-    .then(setTexts)
-    .catch(error => {
-      console.error(error);
-    })
-    ;
-  };
-}
-
-
 ExternalText.contextType = ExternalTextContext;
 ExternalText.Provider = Provider;
 ExternalText.get = get;
 ExternalText.Markdown = ReactMarkdown;
-ExternalText.makeYamlLoader = makeYamlLoader;
