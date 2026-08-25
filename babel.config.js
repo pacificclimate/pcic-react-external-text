819d3dc8e50a2b1555de934109b363a5db3d568a
Quintin <quintins@uvic.ca>
2026-08-25 09:53:06 -0700

    CRA 3, webpack 4 compatibility fix

--- babel.config.js
+++ babel.config.js
@@ -4,10 +4,14 @@ module.exports = {
       '@babel/preset-env',
       {
         targets: {
-          node: 'current',
+          esmodules: true,
         },
       },
     ],
-    '@babel/preset-react'
+    '@babel/preset-react',
   ],
+  // CRA 3/Webpack 4 cannot parse class fields in dependencies, even when the
+  // consuming browser supports them. Keep the target modern while lowering
+  // this syntax in the published library artifacts.
+  plugins: ['@babel/plugin-transform-class-properties'],
 };
